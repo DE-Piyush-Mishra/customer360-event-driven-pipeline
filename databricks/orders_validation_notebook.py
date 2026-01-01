493dc6e73b0a2a2c952dd9787f5c429a594785ec
DE-Piyush-Mishra <jobsformishrapiyush@gmail.com>
2026-01-02 01:59:20 +0530

    Add Databricks notebook structure

---
+++ databricks/orders_validation_notebook.py
@@ -0,0 +1,7 @@
+Databricks notebook used by Azure Data Factory.
+
+This notebook receives the file name dynamically from ADF,
+performs validation checks, and moves the file to staging
+or discarded folders based on the results.
+
+
